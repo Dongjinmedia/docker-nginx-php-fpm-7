c611c7607aaba7c139c9b342df98e993d8511c46
Dongjin Park <dongjinmedia@gmail.com>
2019-09-02 19:17:13 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,3 @@
 <?php
 
-use HelloWorld\Response;
-
-spl_autoload_register(function($class) {
-  $parts = explode('\\', $class);
-  require  realpath(__DIR__ . '/../src/' . join(DIRECTORY_SEPARATOR, $parts) . '.php');
-});
-
-$response = new Response();
-echo $response->render();
+phpinfo();
